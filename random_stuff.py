90b38fdb84bd80d50eff33f4e4d371283bf4ddd5
Darshan.Gaikwad <Darshan.Gaikwad@wolterskluwer.com>
2022-06-27 10:54:38 +0530

    first python file

--- random_stuff.py
+++ random_stuff.py
@@ -1,1 +1,104 @@
-print("Hello World","How are you?",sep="  ",end="Thank You!")
+# print("Hello World","How are you?",sep="  ",end="Thank You!")
+# print("Hello World","How are you?",sep=" /n ",end="Thank You!")
+#print()
+name="Darshan"
+#print(f"Hello {name}. How are you?")
+age=25
+#print("Hello {} . You are {} years old".format(name,age))
+# print(type(age))
+# print(type(name))
+# cars=['audi','Mercedes',10,23,[23,[56]],'scorpio']
+# print(cars)
+# cars[2]='maruti'
+# print(cars)
+# tuple1=(12,23,34,23,'audi','BMW',[23,[34]])
+# print(tuple1)
+#tuple1[2]='darshan'
+# print(tuple1)
+#print(range(5,9,2))
+# player=['John',26, 6.1, "Basket Ball"]
+# player={
+#     "name":"john",
+#     "age":26,
+#     "Height":6.1,
+#     "game":"Basket Ball"
+# }
+# print(player)
+
+# canbeanything={1}
+# print(type(canbeanything))
+
+# lang="32761542765423"
+# print(len(lang))
+# print(lang[2:5])
+# print(lang[2:])
+# num='10'
+# num=int(num)
+# print(num+num)
+
+# height=6.3
+# print(int(height))
+
+# list1=[1,2,3,4,5,]
+# print(tuple(list1))
+# print(list1)
+
+# player={
+#     "name":"John",
+#     "age":25,
+#     "Height":6.1,
+#     "game":"cricket"
+# }
+# print(player["name"])
+# print(player.get("name"))
+
+# blank=(1)
+# if(0):
+#     print("Hii")
+# elif(blank):
+#     print("Hello Darshan")
+# else:
+#     print("no problem")
+
+# counter=0
+# while(counter<10):
+#     print(counter)
+#     counter+=1
+# else:
+#     print("BYE LOOP")
+
+# for _ in range(0,10):
+#     print("HII")
+
+# cars=['audi','BMW']
+# for car in cars:
+#     print(car)
+
+player={
+    "name":"John",
+    "Age":27,
+    "Height":5.5,
+    "Game":"Cricket"
+}
+# print(player.keys())
+# print(player.values())
+
+# for key in player.keys():
+#     print(f"{key}:{player[key]}")
+
+#print(player.items())
+
+for _,y in player.items():
+    print(_,":",y)
+
+
+
+
+
+
+
+
+
+
+
+
