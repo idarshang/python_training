7aeb9980342219b8db8960348e5b933f309e053d
Darshan.Gaikwad <Darshan.Gaikwad@wolterskluwer.com>
2022-06-28 10:23:53 +0530

    first python file all done Higher order functions Map Filter and reduce fns practice vowelcount updated

--- Vowel_count.py
+++ Vowel_count.py
@@ -2,8 +2,30 @@ input_string=input('Enter a string')
 input_string=input_string.casefold()
 print(input_string)
 vowels='aeiou'
-def numofvowels(input):
-    num_vowels=[vowel for vowel in input if vowel in vowels]
-    return  num_vowels
+# vlist=[]
+# for char in input_string:
+#     if char in vowels:
+#         vlist.append(char)
+#
+# print(vlist)
+# num_vowels=[vowel for vowel in input_string if vowel in vowels]
+#
+# print(len(num_vowels),num_vowels)
 
-print(numofvowels(input_string))
+# count number of times each vowel occures
+
+vowel_count = {}.fromkeys(vowels, 0)
+print(vowel_count)
+for char in input_string:
+    if char in vowels:
+        vowel_count[char]+=1
+
+print(vowel_count)
+
+
+
+# def numofvowels(input):
+#     num_vowels=[vowel for vowel in input if vowel in vowels]
+#     return  num_vowels
+#
+# print(numofvowels(input_string))
