f45b1b494b2e0354b155beb61f66c5f3dd28084d
Darshan.Gaikwad <Darshan.Gaikwad@wolterskluwer.com>
2022-08-08 21:47:40 +0530

    PYTHON PRACTICE

---
+++ game/pigame.py
@@ -0,0 +1,21 @@
+scores = [0, 0]
+current_score = 0
+active_player = 0
+playing = True
+
+def init():
+    scores = [0, 0]
+    current_score = 0
+    active_player = 0
+    playing = True
+
+def switch_player():
+    current_score = 0
+    active_player = 1 if active_player == 0 else 1
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
